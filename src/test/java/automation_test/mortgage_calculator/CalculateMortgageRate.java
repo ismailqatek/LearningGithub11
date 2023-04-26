46186fc13b0c40183a0e37ad89048a00628bac1b
Ismail Mohammad Jamal <ami.ismail.268@gmail.com>
2023-04-26 21:17:37 +0800

    Saving Automation Project

--- src/test/java/automation_test/mortgage_calculator/CalculateMortgageRate.java
+++ src/test/java/automation_test/mortgage_calculator/CalculateMortgageRate.java
@@ -1,28 +1,10 @@
 package automation_test.mortgage_calculator;
 
-
-import command_providers.ActOn;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import page_objects.NavigationBar;
 import utilities.DateUtils;
 
-public class CalculateMortgageRate {
-    WebDriver driver;
-
-    @BeforeMethod
-    public void OpenBrowser(){
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(options);
-        ActOn.browser(driver).openBrowser("https://www.mortgagecalculator.org/");
-    }
+public class CalculateMortgageRate extends BaseClass {
 
     @Test
     public void calculateMonthlyPayment() {
@@ -49,8 +31,4 @@ public class CalculateMortgageRate {
 
     }
 
-    @AfterMethod
-    public void closeBrowser(){
-       driver.quit();
-    }
 }
